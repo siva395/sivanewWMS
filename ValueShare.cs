f70f261e60c7558f866d8efff7135c088c24afff
siva395 <86281831+siva395@users.noreply.github.com>
2021-06-22 11:20:08 +0530

    final

---
+++ ValueShare.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WMS.Client
+{
+    public  class ValueShare
+    {
+        public  string ComponentNameShare { get; set; }
+    }
+}
